5eba19fd9b6f706fbee6d788b0ec491f2a4b4be5
Torresypther <torresypther@gmail.com>
2025-05-07 16:03:58 +0800

    updated

--- churn_prediction_api.py
+++ churn_prediction_api.py
@@ -16,26 +16,39 @@ api = Flask(__name__)
 CORS(api)
 
 @api.route('/api/customer_churn_prediction', methods=['POST'])
-def heart_failure_prediction():
-    data = request.json['inputs']
-    input_df = pd.DataFrame(data)
-    input_encoded = encoder.transform(input_df[categorical_features])
-    input_df = input_df.drop(categorical_features, axis=1)
-    input_encoded = input_encoded.reset_index(drop=True)
-    
-    final_input = pd.concat([input_df, input_encoded], axis=1)
-    
-    prediction = model.predict_proba(final_input)
-    class_labels = model.classes_
-    
-    response = []
-    for prob in prediction:
-        prob_dict = {}
-        for k, v in zip(class_labels, prob):
-            prob_dict[str(k)] = round(float(v) * 100, 2)
-        response.append(prob_dict)
-    
-    return jsonify({'prediction': response})
+def customer_churn_prediction():
+    try:
+        data = request.json['inputs']
+        input_df = pd.DataFrame(data)
+        input_encoded = encoder.transform(input_df[categorical_features])
+        input_df = input_df.drop(categorical_features, axis=1)
+        input_encoded = input_encoded.reset_index(drop=True)
+        
+        final_input = pd.concat([input_df, input_encoded], axis=1)
+        
+        prediction = model.predict_proba(final_input)
+        class_labels = model.classes_
+        
+        response = []
+        for prob in prediction:
+            prob_dict = {}
+            for k, v in zip(class_labels, prob):
+                prob_dict[str(k)] = round(float(v) * 100, 2)
+            response.append(prob_dict)
+        
+        return jsonify({
+            'status': 'success',
+            'message': 'Prediction generated successfully.',
+            'data': {
+                'predictions': response
+            }
+        }), 200
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An error occurred while processing the request.',
+            'error': str(e)
+        }), 500
 
 if __name__ == '__main__':
     api.run(port=8000)
